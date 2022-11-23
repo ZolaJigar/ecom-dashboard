d25dbb7d0d3b0204bccfa3b34c62d95228e42e82
zolajigar <kingofdarkness012345@gmail.com>
2022-11-23 11:58:32 +0530

    Logout button added and username of logged in person shwoing into navbar

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,15 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 function Header() {
+    const navigate = useNavigate();
+    const username = JSON.parse(localStorage.getItem('user-info'));
+    console.log(username);
+
+    function logout() {
+        localStorage.clear();
+        navigate("/login");
+
+    }
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -18,7 +27,14 @@ function Header() {
                                 <Link to="/register"> Register</Link>
                             </>
                     }
-
+                </Nav>
+                <Nav>
+                    {
+                        localStorage.getItem('user-info') ?
+                            <NavDropdown title={username && username.name}>
+                                <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                            </NavDropdown> : ""
+                    }
                 </Nav>
             </Container>
         </Navbar>
